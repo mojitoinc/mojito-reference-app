ba54ee2f5bd590a65f5716ce16cf4587032ad8a3
robdev91 <robert@mojito.xyz>
2022-04-26 12:31:37 -0400

    simplify if statement

--- src/services/hasAccessToToken.tsx
+++ src/services/hasAccessToToken.tsx
@@ -1,11 +1,7 @@
 // TODO: write detailed implementation
 
 const hasAccessToToken = (token: string): boolean => {
-  if (token === "1234567890" || token === "1234567899") {
-    return true;
-  } else {
-    return false;
-  }
+  return token === "1234567890" || token === "1234567899";
 };
 
 export default hasAccessToToken;
